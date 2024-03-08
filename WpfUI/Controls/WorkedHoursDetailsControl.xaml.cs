8d6b9de105c391f7bae5a84634bd4d3c905d45fe
Stevendevrieswork <steven.d.vries.work@gmail.com>
2024-03-08 23:05:26 +0100

    Finally Client Job updated

--- WpfUI/Controls/WorkedHoursDetailsControl.xaml.cs
+++ WpfUI/Controls/WorkedHoursDetailsControl.xaml.cs
@@ -12,11 +12,11 @@ namespace WpfUI.Controls
         public WorkedHoursDetailsControl()
         {
             InitializeComponent();
-            GlobalsViewModel.Clients.CollectionChanged += Data_CollectionChanged;
+            GlobalsViewModel.JobsClients.CollectionChanged += Data_CollectionChanged;
         }
         private void Data_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            ComboBoxJobs.SelectedIndex = 0;
+            ComboBoxJobs.SelectedIndex = GlobalsViewModel.SelectedJobId;
         }
 
     }
